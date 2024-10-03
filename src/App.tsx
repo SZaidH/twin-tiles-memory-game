d8f6e9c755549c8e0ed11a03ca74f97db5f6796b
Zaid Hasan <szh619@gmail.com>
2024-10-03 11:20:43 +0300

    Minor CSS changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="container flex flex-col justify-center items-center text-center">
+    <div className="flex flex-col justify-center items-center text-center">
       <Header />
       <Game />
     </div>
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,18 +5,41 @@ const Game = () => {
   return (
     <main>
       <ResetGame />
-      <div className="card-container flex flex-row">
-        <div className="card w-56 h-64 m-5">
+      <div className="card-container grid grid-cols-4 gap-6">
+        <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
             <img src={bgPolygon} alt="Card Front" />
           </div>
         </div>
-        <div className="card w-56 h-64 m-5">
+        <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
             <img src={bgPolygon} alt="Card Front" />
           </div>
         </div>
-        <div className="card w-56 h-64 m-5">
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-front">
+            <img src={bgPolygon} alt="Card Front" />
+          </div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-front">
+            <img src={bgPolygon} alt="Card Front" />
+          </div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-front">
+            <img src={bgPolygon} alt="Card Front" />
+          </div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-front">
+            <img src={bgPolygon} alt="Card Front" />
+          </div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-back h-full bg-primary flex justify-center align-middle"></div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
             <img src={bgPolygon} alt="Card Front" />
           </div>
