b4b1dd1be9074231b2fa1e647c67866777a4f53c
Zaid Hasan <szh619@gmail.com>
2024-10-06 13:49:54 +0300

    Added art assets

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,10 @@
 import ResetGame from "./ResetGame";
-import bgPolygon from "../assets/bg-polygon.png";
+import cardBack from "../assets/card-back.png";
+import cardSword from "../assets/card-sword.png";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import cardHammer from "../assets/card-hammer.png";
+import cardCoin from "../assets/card-coin.png";
+import cardDiamond from "../assets/card-diamond.png";
 
 const Game = () => {
   return (
@@ -7,41 +12,43 @@ const Game = () => {
       <ResetGame />
       <div className="card-container grid grid-cols-4 gap-6">
         <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
+          <div className="card-back">
+            <img src={cardBack} alt="Card Front" />
           </div>
         </div>
         <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
+            <img src={cardCoin} alt="Card Front" />
+          </div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-back">
+            <img src={cardBack} alt="Card Front" />
           </div>
         </div>
         <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
+            <img src={cardSword} alt="Card Front" />
           </div>
         </div>
         <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
+            <img src={cardCoin} alt="Card Front" />
+          </div>
+        </div>
+        <div className="card w-56 h-64 drop-shadow-2xl">
+          <div className="card-back">
+            <img src={cardBack} alt="Card Front" />
           </div>
         </div>
         <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
+            <img src={cardBack} alt="Card Front" />
           </div>
         </div>
         <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
-          </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-back h-full bg-primary flex justify-center align-middle"></div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={bgPolygon} alt="Card Front" />
+            <img src={cardSword} alt="Card Front" />
           </div>
         </div>
       </div>
