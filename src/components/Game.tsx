db2c0186783331fc30e1da09d49697524ece01d1
Zaid Hasan <szh619@gmail.com>
2024-10-07 16:19:15 +0300

    Implemented Fisher-Yates Shuffle

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,56 +1,42 @@
-import ResetGame from "./ResetGame";
 import cardBack from "../assets/card-back.png";
 import cardSword from "../assets/card-sword.png";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import cardHammer from "../assets/card-hammer.png";
 import cardCoin from "../assets/card-coin.png";
 import cardDiamond from "../assets/card-diamond.png";
+import ResetGame from "./ResetGame";
+import { SortCards } from "../utils/SortCards";
+import { useEffect, useState } from "react";
 
 const Game = () => {
+  const [cards, setCards] = useState<string[]>([
+    cardSword,
+    cardHammer,
+    cardCoin,
+    cardDiamond,
+  ]);
+
+  useEffect(() => {
+    const initialCards = [...cards, ...cards];
+    const sortedCards = SortCards(initialCards);
+    setCards(sortedCards);
+  }, []);
+
   return (
     <main>
       <ResetGame />
       <div className="card-container grid grid-cols-4 gap-6">
-        <div className="card w-56 h-64 drop-shadow-2xl">
+        {/* <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-back">
-            <img src={cardBack} alt="Card Front" />
+            <img src={cardBack} alt="Card Back" />
           </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={cardCoin} alt="Card Front" />
+        </div> */}
+        {cards.map((card, index) => (
+          <div key={index} className="card w-56 h-64 drop-shadow-2xl">
+            <div className="card-front">
+              <img src={card} alt="Card Front" />
+            </div>
           </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-back">
-            <img src={cardBack} alt="Card Front" />
-          </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={cardSword} alt="Card Front" />
-          </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={cardCoin} alt="Card Front" />
-          </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-back">
-            <img src={cardBack} alt="Card Front" />
-          </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={cardBack} alt="Card Front" />
-          </div>
-        </div>
-        <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-front">
-            <img src={cardSword} alt="Card Front" />
-          </div>
-        </div>
+        ))}
       </div>
     </main>
   );
---
+++ src/utils/SortCards.ts
@@ -0,0 +1,8 @@
+//Implementing Fisher-Yates Shuffle
+export const SortCards = (array: string[]): string[] => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+};
