a79d3cd8a09421f477ff314352131a5fc82e5e44
Zaid Hasan <szh619@gmail.com>
2024-10-08 12:07:43 +0300

    Tile shuffle button functionality

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -16,14 +16,14 @@ const Game = () => {
   ]);
 
   useEffect(() => {
-    const initialCards = [...cards, ...cards];
-    const sortedCards = SortCards(initialCards);
+    const initialCards: string[] = [...cards, ...cards];
+    const sortedCards: string[] = SortCards(initialCards);
     setCards(sortedCards);
   }, []);
 
   return (
     <main>
-      <ResetGame />
+      <ResetGame cards={cards} setCards={setCards} />
       <div className="card-container grid grid-cols-4 gap-6">
         {/* <div className="card w-56 h-64 drop-shadow-2xl">
           <div className="card-back">
@@ -33,7 +33,7 @@ const Game = () => {
         {cards.map((card, index) => (
           <div key={index} className="card w-56 h-64 drop-shadow-2xl">
             <div className="card-front">
-              <img src={card} alt="Card Front" />
+              <img src={card} alt={`Card ${index}`} />
             </div>
           </div>
         ))}
--- src/components/ResetGame.tsx
+++ src/components/ResetGame.tsx
@@ -1,6 +1,23 @@
-const ResetGame = () => {
+import { SortCards } from "../utils/SortCards";
+
+interface ResetGameProps {
+  cards: string[];
+  setCards: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const ResetGame: React.FC<ResetGameProps> = ({ cards, setCards }) => {
+  //Function to shuffle the cards
+  const handleReset = () => {
+    const resetCards = [...cards];
+    setCards(SortCards(resetCards));
+    console.log("hello");
+  };
+
   return (
-    <button className="p-2 font-secondary bg-primary text-white text-2xl rounded-md mb-6">
+    <button
+      className="p-2 font-secondary bg-primary text-white text-2xl rounded-md mb-6"
+      onClick={handleReset}
+    >
       Reset Game
     </button>
   );
