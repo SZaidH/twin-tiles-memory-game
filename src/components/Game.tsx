d3344056e1c9c16a00e4290b7a36277a0a91f981
Zaid Hasan <szh619@gmail.com>
2024-10-08 15:54:17 +0300

    Added Tile flip functionality

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,36 +5,44 @@ import cardCoin from "../assets/card-coin.png";
 import cardDiamond from "../assets/card-diamond.png";
 import ResetGame from "./ResetGame";
 import { SortCards } from "../utils/SortCards";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Game = () => {
-  const [cards, setCards] = useState<string[]>([
-    cardSword,
-    cardHammer,
-    cardCoin,
-    cardDiamond,
+  //Duplicating the cards and shuffling them using the SortCards utility function
+  const initialCards: string[] = SortCards([
+    ...Array(2).fill([cardSword, cardHammer, cardCoin, cardDiamond]).flat(),
   ]);
+  const [cards, setCards] = useState<string[]>(initialCards);
+  // State for tracking flip status of cards
+  const [flipped, setFlipped] = useState<boolean[]>(
+    new Array(initialCards.length).fill(false)
+  );
 
-  useEffect(() => {
-    const initialCards: string[] = [...cards, ...cards];
-    const sortedCards: string[] = SortCards(initialCards);
-    setCards(sortedCards);
-  }, []);
+  const handleCardClick = (index: number) => {
+    const cardFlip: boolean[] = [...flipped];
+    cardFlip[index] = !cardFlip[index];
+    setFlipped(cardFlip);
+  };
 
   return (
     <main>
       <ResetGame cards={cards} setCards={setCards} />
       <div className="card-container grid grid-cols-4 gap-6">
-        {/* <div className="card w-56 h-64 drop-shadow-2xl">
-          <div className="card-back">
-            <img src={cardBack} alt="Card Back" />
-          </div>
-        </div> */}
         {cards.map((card, index) => (
-          <div key={index} className="card w-56 h-64 drop-shadow-2xl">
-            <div className="card-front">
-              <img src={card} alt={`Card ${index}`} />
-            </div>
+          <div
+            key={index}
+            onClick={() => handleCardClick(index)}
+            className="card w-56 h-64 drop-shadow-2xl"
+          >
+            {!flipped[index] ? (
+              <div className="card-back">
+                <img src={cardBack} alt="Card Back" />
+              </div>
+            ) : (
+              <div className="card-front">
+                <img src={card} alt={`Card ${index}`} />
+              </div>
+            )}
           </div>
         ))}
       </div>
